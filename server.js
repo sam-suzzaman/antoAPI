ed985442e4555b64b8bf324339f5726f7736bd56
sam-suzzaman <dksamsuzzaman@gmail.com>
2024-09-19 12:54:37 +0600

    initialize

---
+++ server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const app = require("./app");
+
+// DB Connection
+
+// Server
+const port = process.env.PORT || 2000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
